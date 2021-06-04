f8d88aef91c096d4608c8370a3ef7d27384c003c
Jon Paul Miles <codingfriend1@gmail.com>
2021-06-04 12:23:10 -0500

    Fixes folder.js

--- the-magic/build/optimize-images.js
+++ the-magic/build/optimize-images.js
@@ -2,12 +2,12 @@
  * Gathers fullsized images in the designated images folder and reduces their filesize for use on the web and saves them ot the designated static folder using the same structure they had in the images folder
  */
 
-const path = require('path')
+const path = require("path")
 const imagemin = require("imagemin-keep-folder")
-const imageminJpegRecompress = require('imagemin-jpeg-recompress')
+const imageminJpegRecompress = require("imagemin-jpeg-recompress")
 const webp = require("imagemin-webp")
-const imageminOptipng = require('imagemin-optipng')
-const folders = require('./folder.js')
+const imageminOptipng = require("imagemin-optipng")
+const folders = require("./folders.js")
 
 const PNG = path.join(folders.images_folder, `**`, `*.png`)
 const JPG = path.join(folders.images_folder, `**`, `*.jpg`)
@@ -16,50 +16,50 @@ const JPEG = path.join(folders.images_folder, `**`, `*.jpeg`)
 imagemin([PNG], {
   use: [
     webp({
-      lossless: true
-    })
+      lossless: true,
+    }),
   ],
-  replaceOutputDir: output => {
+  replaceOutputDir: (output) => {
     return path.join(folders.static_folder, path.basename(output))
-  }
-});
+  },
+})
 
 imagemin([PNG], {
   use: [
     imageminOptipng({
-      optimizationLevel: 3
-    })
+      optimizationLevel: 3,
+    }),
   ],
-  replaceOutputDir: output => {
+  replaceOutputDir: (output) => {
     return path.join(folders.static_folder, path.basename(output))
-  }
-});
+  },
+})
 
 imagemin([JPG, JPEG], {
   use: [
     webp({
-      quality: 80 // Quality setting from 0 to 100
-    })
+      quality: 80, // Quality setting from 0 to 100
+    }),
   ],
-  replaceOutputDir: output => {
+  replaceOutputDir: (output) => {
     return path.join(folders.static_folder, path.basename(output))
-  }
-});
+  },
+})
 
 imagemin([JPG, JPEG], {
   use: [
     imageminJpegRecompress({
       accurate: true,
-      quality: 'high',
+      quality: "high",
       min: 50,
       max: 90,
       loops: 6,
       progressive: true,
       strip: true,
-      target: 0.9
-    })
+      target: 0.9,
+    }),
   ],
-  replaceOutputDir: output => {
+  replaceOutputDir: (output) => {
     return path.join(folders.static_folder, path.basename(output))
-  }
-});
+  },
+})
